094ef0d7ab9b143dacb758ba98072fa25e4bc2e0
Radoclav Mladenovic <radoclav.mladenovic@gmail.com>
2020-11-10 14:57:53 +0100

    create login.php and update index.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f1942ef97e6bd43d2f9783b0e575d359b8a5e485',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f1942ef97e6bd43d2f9783b0e575d359b8a5e485',
+    ),
+  ),
+);
--- web/login.php
+++ web/login.php
@@ -1,20 +1,14 @@
 <?php
-// index.php
-// ...
+// login.php
 require '../vendor/autoload.php';
 use Auth0\SDK\Auth0;
 $auth0 = new Auth0([
   'domain' => 'test-silex-labs-rado.eu.auth0.com',
   'client_id' => 'IHKNEjr02YDQWaQzLB2ZdduaXMf31VaT',
   'client_secret' => 'SgxxK4gapuREY4_Iq7gi1JiG47bH20WS0z8kjiZ6CGBqRliaHFjufPiSe2TqBjqk',
-  'redirect_uri' => 'http://localhost:3000/',
+  'redirect_uri' => 'https://YOUR_APP/callback',
   'scope' => 'openid profile email',
 ]);
+$auth0->login();
 $userInfo = $auth0->getUser();
-if (!$userInfo) {
-    // We have no user info
-    // See below for how to add a login link
-} else {
-    // User is authenticated
-    // See below for how to display user information
-}
+printf( 'Hello %s!', htmlspecialchars( $userInfo['name'] ) );
