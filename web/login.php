97560ccee59930694a1bf4588dc319cf55e05a46
Radoslav79 <radoclav.mladenovic@gmail.com>
2020-12-01 11:52:29 +0100

    nouveau login

--- web/login.php
+++ web/login.php
@@ -1,14 +1,32 @@
-<?php
-// login.php
-require '../vendor/autoload.php';
-use Auth0\SDK\Auth0;
-$auth0 = new Auth0([
-  'domain' => 'test-silex-labs-rado.eu.auth0.com',
-  'client_id' => 'IHKNEjr02YDQWaQzLB2ZdduaXMf31VaT',
-  'client_secret' => 'SgxxK4gapuREY4_Iq7gi1JiG47bH20WS0z8kjiZ6CGBqRliaHFjufPiSe2TqBjqk',
-  'redirect_uri' => 'https://localhost:3000/',
-  'scope' => 'openid profile email',
-]);
-$auth0->login();
-$userInfo = $auth0->getUser();
-printf( 'Hello %s!', htmlspecialchars( $userInfo['name'] ) );
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+    
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Pseudo</b></label>
+                <input type="text" placeholder="Entrer le pseudo" name="username" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="mot_de_passe" required>
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+        </div>
+    </body>
+</html>
